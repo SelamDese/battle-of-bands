2d18e6b80cb4c0514185cb352ca29e78f9eafedc
selam <selam0315@gmail.com>
2018-12-06 02:10:22 -0600

    takeNumber function setup

---
+++ main.js
@@ -0,0 +1,18 @@
+let bandNumber = 1
+
+const takeNumber = function (bandName) {
+    /*
+        Write your awesome code here. See comments
+        below for what should be returned.
+    */
+   for (let i=0; i<bandName.length; i++){
+       console.log(bandNumber+'. '+bandName)
+       bandNumber ++
+   }
+}
+
+const scum = takeNumber("Galactic Scum")
+console.log(scum)  // This should print "1. Galactic Scum" in the console
+
+const under = takeNumber("Underdogs")
+console.log(under)  // This should print "2. Underdogs" in the console
